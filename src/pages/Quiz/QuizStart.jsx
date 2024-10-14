1ec6639ed17e1c1b3b82c3f1a88405c042b6916b
joshmoon827 <joshmoon827@gmail.com>
2024-10-14 19:12:08 +0900

    fix :: title
    asdf

--- src/pages/Quiz/QuizStart.jsx
+++ src/pages/Quiz/QuizStart.jsx
@@ -2,10 +2,30 @@ import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import * as M from "./QuizStyle";
+import Masonry from "react-masonry-css";
 import Spline from "@splinetool/react-spline";
 
 const QuizStart = () => {
   const [points, setPoints] = useState(2043);
+  var items = [
+    { id: 1, name: "My First Item" },
+    { id: 2, name: "Another item" },
+    { id: 3, name: "Third Item" },
+    { id: 4, name: "Here is the Fourth" },
+    { id: 5, name: "High Five" },
+  ];
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
+  // Convert array to JSX items
+  items = items.map(function (item) {
+    return <M.PointContainer key={item.id}>{item.name}</M.PointContainer>;
+  });
+
   return (
     <div>
       <React.Fragment>
@@ -13,14 +33,24 @@ const QuizStart = () => {
           <Header />
           <M.GlobalStyles />
           <M.PageContent>
-              <Sidebar />
+            <Sidebar />
             <M.QuizMainContainer>
               <M.TestContainer>
                 <M.MiniTxt>닉네임님</M.MiniTxt>
-                <M.LargeTitle>미나리 경제 상식 퀴즈로 닉네임님의 <br/> 경제 지식을 평가해보세요!</M.LargeTitle>
+                <M.LargeTitle>
+                  미나리 경제 상식 퀴즈로 닉네임님의 <br /> 경제 지식을
+                  평가해보세요!
+                </M.LargeTitle>
                 <M.SimButton to="/quiz">테스트 하러 가기</M.SimButton>
               </M.TestContainer>
-              
+              //...
+              <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+              >
+                {items}
+              </Masonry>
             </M.QuizMainContainer>
           </M.PageContent>
         </M.Zindex>
