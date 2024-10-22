cb108a2a74f9e02fdea647c4581b31a068f41fa1
0ccssu <kimsuaa0114@dgsw.hs.kr>
2024-10-23 00:28:36 +0900

    feat :: Grapes 서버통신
    Grapes 서버통신

--- src/pages/Tutorial/Grapes/Grapes.jsx
+++ src/pages/Tutorial/Grapes/Grapes.jsx
@@ -8,8 +8,10 @@ import * as M from "./GrapesStyle";
 const Grapes = () => {
   const navigate = useNavigate();
   const [data, setData] = useState(null);
+  const [grapeData, setGrapeData] = useState([]);
   const [error, setError] = useState(null);
-  const gpsId = 8;
+  const gpsId = 8; // GPS ID
+  const gpId = 9; // Grape ID
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +38,24 @@ const Grapes = () => {
           setData(response.data.data);
         } else {
           setError(response.data.message);
+        }
+
+        // 포도알 데이터 요청
+        const grapeResponse = await axios.get(
+          `https://cheongfordo.kr/gp/${gpId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${storedToken}`,
+            },
+          }
+        );
+
+        console.log("Grape API Response:", grapeResponse.data);
+
+        if (grapeResponse.data.status === 200) {
+          setGrapeData(grapeResponse.data.data);
+        } else {
+          setError(grapeResponse.data.message);
         }
       } catch (err) {
         console.error("Error fetching data:", err);
@@ -113,12 +133,98 @@ const Grapes = () => {
                       <M.GrapeExplanation>{data.gpsContent}</M.GrapeExplanation>
                     </M.TextContainer>
                   </M.ContentContainer>
-                  <M.NextBtn>다음 {">"}</M.NextBtn>
+                  <M.NextBtn onClick={() => navigate("/grapeseed")}>
+                    다음 {">"}
+                  </M.NextBtn>
                 </M.DataContainer>
               ) : (
                 <M.LoadingMessage>로딩 중...</M.LoadingMessage>
               )}
-              <M.GrapeContainer></M.GrapeContainer>
+              <M.GrapeContainer>
+                <M.TitleContainer>
+                  <M.DetailGrapeImg />
+                  <M.GrapeTextContainer>
+                    <M.DetailGrapeTitle>
+                      시작하기: 우리가 경제를 배워야하는 이유
+                    </M.DetailGrapeTitle>
+                    <M.DetailGrapeExplanation>
+                      경제를 알아보기 전에 국가에서 실시하는 나이별 경제
+                      제도를...
+                    </M.DetailGrapeExplanation>
+                  </M.GrapeTextContainer>
+                </M.TitleContainer>
+                <M.DetailBtnContainer>
+                  <M.NextBtn onClick={() => navigate("/grapeseed")}>
+                    다음 {">"}
+                  </M.NextBtn>
+                </M.DetailBtnContainer>
+                <M.SequenceContainer>
+                  <M.SequenceText>목차</M.SequenceText>
+                  {grapeData.length > 0 ? (
+                    grapeData.map((grape, index) => (
+                      <M.DataContainer key={grape.gpseId}>
+                        <M.GrapeInfoContainer>
+                          <M.SmallGrapeTitle>{`${index + 1}. ${
+                            grape.gpseNm
+                          }`}</M.SmallGrapeTitle>
+                          <M.Details>
+                            <M.TimeText>{grape.gpseTm}분</M.TimeText>
+                            <M.EndOrNot>
+                              {grape.gpseTF ? "완료" : "미완료"}
+                            </M.EndOrNot>
+                          </M.Details>
+                        </M.GrapeInfoContainer>
+                      </M.DataContainer>
+                    ))
+                  ) : (
+                    <M.LoadingMessage>
+                      포도알 데이터 로딩 중...
+                    </M.LoadingMessage>
+                  )}
+                </M.SequenceContainer>
+              </M.GrapeContainer>
+              <M.GrapeContainer>
+                <M.TitleContainer>
+                  <M.DetailGrapeImg />
+                  <M.GrapeTextContainer>
+                    <M.DetailGrapeTitle>
+                      10대 - 주니어 투자상품 (청소년 주식 계좌)
+                    </M.DetailGrapeTitle>
+                    <M.DetailGrapeExplanation>
+                      주니어 투자 상품이란?
+                    </M.DetailGrapeExplanation>
+                  </M.GrapeTextContainer>
+                </M.TitleContainer>
+                <M.DetailBtnContainer>
+                  <M.NextBtn onClick={() => navigate("/grapeseed")}>
+                    다음 {">"}
+                  </M.NextBtn>
+                </M.DetailBtnContainer>
+                <M.SequenceContainer>
+                  <M.SequenceText>목차</M.SequenceText>
+                  {grapeData.length > 0 ? (
+                    grapeData.map((grape, index) => (
+                      <M.DataContainer key={grape.gpseId}>
+                        <M.GrapeInfoContainer>
+                          <M.SmallGrapeTitle>{`${index + 1}. ${
+                            grape.gpseNm
+                          }`}</M.SmallGrapeTitle>
+                          <M.Details>
+                            <M.TimeText>{grape.gpseTm}분</M.TimeText>
+                            <M.EndOrNot>
+                              {grape.gpseTF ? "완료" : "미완료"}
+                            </M.EndOrNot>
+                          </M.Details>
+                        </M.GrapeInfoContainer>
+                      </M.DataContainer>
+                    ))
+                  ) : (
+                    <M.LoadingMessage>
+                      포도알 데이터 로딩 중...
+                    </M.LoadingMessage>
+                  )}
+                </M.SequenceContainer>
+              </M.GrapeContainer>
             </M.GrapesContainer>
           </M.CenteredContent>
         </M.MainContent>
