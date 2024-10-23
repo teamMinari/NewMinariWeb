de433cc38bf89ac5f1ff46fc156ad78389b0a391
0ccssu <kimsuaa0114@dgsw.hs.kr>
2024-10-23 09:57:05 +0900

    feat :: Termmining 용어 일일 조회
    Termmining 용어 일일 조회

--- src/pages/Dictionary/Dictionary.jsx
+++ src/pages/Dictionary/Dictionary.jsx
@@ -6,8 +6,8 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import Term from "../../components/Term/Term";
 import axios from "axios";
 import * as gvar from "../../common/global_variables";
-import Spinner from '../Home/Spinner';
-import { useNavigate } from "react-router-dom"; // react-router-dom 추가
+import Spinner from "../Home/Spinner";
+import { useNavigate } from "react-router-dom";
 
 const Dictionary = () => {
   const [terms, setTerms] = useState([]);
@@ -15,7 +15,7 @@ const Dictionary = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [termsPerPage] = useState(13);
   const token = String(localStorage.getItem("accessToken"));
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTerms = async (page = 0, size = 400) => {
@@ -83,7 +83,6 @@ const Dictionary = () => {
     return text.length > maxChars ? text.slice(0, maxChars) + "..." : text;
   };
 
-  // 현재 페이지의 용어 목록을 가져오는 함수
   const currentTerms = filteredTerms.slice(
     currentPage * termsPerPage,
     (currentPage + 1) * termsPerPage
@@ -92,8 +91,7 @@ const Dictionary = () => {
   const totalPages = Math.ceil(filteredTerms.length / termsPerPage);
 
   const handleTermClick = (term) => {
-    // 용어 클릭 시 TermMeaning으로 네비게이션
-    navigate(`/term-meaning`, { state: { term } });
+    navigate("/termmeaning", { state: { term } });
   };
 
   return (
@@ -147,11 +145,11 @@ const Dictionary = () => {
                     title={term.title}
                     explanation={renderExplanation(term.explanation)}
                     difficulty={term.difficulty}
-                    onClick={() => handleTermClick(term)} // 클릭 이벤트 핸들러 추가
+                    onClick={() => handleTermClick(term)}
                   />
                 ))
               ) : (
-                <Spinner/>
+                <Spinner />
               )}
               <M.PaginationContainer>
                 <M.PaginationButton
